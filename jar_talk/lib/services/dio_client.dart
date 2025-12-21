0229e4807dc85ba7931cda1a39872f24117346a1
jinergenkai <jinergenkai@gmail.com>
2025-12-21 17:04:23 +0700

    deploy android

--- jar_talk/lib/services/dio_client.dart
+++ jar_talk/lib/services/dio_client.dart
@@ -9,7 +9,7 @@ class DioClient {
 
   // Android Emulator calls localhost via 10.0.2.2
   // For physical device, use your machine's IP
-  final String _baseUrl = "http://192.168.0.106:8000";
+  final String _baseUrl = "http://192.168.0.100:8000";
 
   DioClient._internal() {
     _dio = Dio(
